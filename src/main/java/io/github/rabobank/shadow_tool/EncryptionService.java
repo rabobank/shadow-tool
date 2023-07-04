b3a60128d61277c8808d4961a0045e90f4961dce
Bradley Meintjes <bradmeintjes@gmail.com>
2023-07-04 11:18:04 +0200

    Mark Encryption service as functional and update documentation

--- src/main/java/io/github/rabobank/shadow_tool/EncryptionService.java
+++ src/main/java/io/github/rabobank/shadow_tool/EncryptionService.java
@@ -1,5 +1,15 @@
 package io.github.rabobank.shadow_tool;
 
+@FunctionalInterface
 interface EncryptionService {
+    /**
+     * Encrypts the given data for logging.
+     * *Note* The result of this is directly logged and should
+     * therefore be in a printable format which meets the minimum encryption requirements of
+     * the specific data.
+     *
+     * @param value the data to encrypt
+     * @return encrypted data
+     */
     String encrypt(final String value);
 }
