0acabc27c32dff392936fe84be8e92f65f80d6c2
Martin Visser <martin.visser01@rabobank.nl>
2023-11-14 20:35:47 +0100

    Noop encryption should encode to base64

--- src/test/java/io/github/rabobank/shadow_tool/NoopEncryptionServiceTest.java
+++ src/test/java/io/github/rabobank/shadow_tool/NoopEncryptionServiceTest.java
@@ -1,19 +1,24 @@
 package io.github.rabobank.shadow_tool;
 
+import org.bouncycastle.util.encoders.Base64;
 import org.junit.jupiter.api.Test;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NoopEncryptionServiceTest {
     @Test
-    void valuesAreNotEncrypted() {
+    void encryptAndDecrypt() {
         final var encryptionService = NoopEncryptionService.INSTANCE;
         final var plainDifferences = "'place' changed: 'Dintelooord' -> 'Dinteloord'\n" +
                                      "'madrigals' collection changes :\n" +
                                      "   1. 'Bruno' changed to 'Mirabel'\n" +
                                      "   0. 'Bruno' added";
         final var encryptedDifferences = encryptionService.encrypt(plainDifferences);
+        //Decrypt and verify
+        final var decoded = Base64.decode(encryptedDifferences);
+        final var result = new String(decoded, UTF_8);
 
-        assertEquals(plainDifferences, encryptedDifferences);
+        assertEquals(plainDifferences, result);
     }
 }
