411b9f541553d95ddd6492279b5b02be23033f69
Visser, M (Martin) <martin.visser01@rabobank.nl>
2026-01-13 16:07:14 +0100

    Refactor DummyObject to use record and improve SameThreadExecutorService with locking mechanism

--- src/test/java/io/github/rabobank/shadow_tool/DummyObject.java
+++ src/test/java/io/github/rabobank/shadow_tool/DummyObject.java
@@ -2,14 +2,11 @@ package io.github.rabobank.shadow_tool;
 
 import java.util.List;
 
-public class DummyObject {
-    private final String name;
-    private final String place;
-    private final List<String> madrigals;
+public record DummyObject(String name, String place, List<String> madrigals) {
 
     public DummyObject(final String name, final String place, final List<String> madrigals) {
         this.name = name;
         this.place = place;
-        this.madrigals = madrigals;
+        this.madrigals = List.copyOf(madrigals);
     }
 }
