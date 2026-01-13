04dbd9a71712fcac173d7664b70b81cd5598cb19
Visser, M (Martin) <martin.visser01@rabobank.nl>
2026-01-13 16:07:14 +0100

    Refactor DummyObject to use record and improve SameThreadExecutorService with locking mechanism

--- src/test/java/io/github/rabobank/shadow_tool/SameThreadExecutorService.java
+++ src/test/java/io/github/rabobank/shadow_tool/SameThreadExecutorService.java
@@ -5,45 +5,131 @@ import org.jspecify.annotations.NonNull;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.AbstractExecutorService;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Executes all submitted tasks directly in the same thread as the caller.
  * Humbly copied from <a href="https://stackoverflow.com/a/13726463">StackOverflow</a>
  */
-public class SameThreadExecutorService extends AbstractExecutorService {
-
-    private volatile boolean terminated;
+class SameThreadExecutorService extends AbstractExecutorService {
+    /**
+     * Lock used whenever accessing the state variables
+     * (runningTasks, shutdown, terminationCondition) of the executor
+     */
+    private final Lock lock = new ReentrantLock();
+    /** Signaled after the executor is shutdown and running tasks are done */
+    private final java.util.concurrent.locks.Condition termination = lock.newCondition();
+    /*
+     * Conceptually, these two variables describe the executor being in
+     * one of three states:
+     *   - Active: shutdown == false
+     *   - Shutdown: runningTasks > 0 and shutdown == true
+     *   - Terminated: runningTasks == 0 and shutdown == true
+     */
+    private int runningTasks = 0;
+    private boolean shutdown = false;
 
     @Override
-    public void shutdown() {
-        terminated = true;
+    public void execute(final Runnable command) {
+        startTask();
+        try {
+            command.run();
+        } finally {
+            endTask();
+        }
     }
 
     @Override
     public boolean isShutdown() {
-        return terminated;
+        lock.lock();
+        try {
+            return shutdown;
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
-    public boolean isTerminated() {
-        return terminated;
-    }
-
-    @Override
-    public boolean awaitTermination(final long timeout, final @NonNull TimeUnit unit) {
-        shutdown(); // TODO ok to call shutdown? what if the client never called shutdown???
-        return terminated;
+    public void shutdown() {
+        lock.lock();
+        try {
+            shutdown = true;
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
     @NonNull
     public List<Runnable> shutdownNow() {
+        shutdown();
         return Collections.emptyList();
     }
 
     @Override
-    public void execute(final Runnable theCommand) {
-        theCommand.run();
+    public boolean isTerminated() {
+        lock.lock();
+        try {
+            return shutdown && runningTasks == 0;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public boolean awaitTermination(final long timeout, final TimeUnit unit)
+            throws InterruptedException {
+        long nanos = unit.toNanos(timeout);
+        lock.lock();
+        try {
+            for (; ; ) {
+                if (isTerminated()) {
+                    return true;
+                } else if (nanos <= 0) {
+                    return false;
+                } else {
+                    nanos = termination.awaitNanos(nanos);
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * Checks if the executor has been shut down and increments the running
+     * task count.
+     *
+     * @throws RejectedExecutionException if the executor has been previously
+     *         shutdown
+     */
+    private void startTask() {
+        lock.lock();
+        try {
+            if (isShutdown()) {
+                throw new RejectedExecutionException("Executor already shutdown");
+            }
+            runningTasks++;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * Decrements the running task count.
+     */
+    private void endTask() {
+        lock.lock();
+        try {
+            runningTasks--;
+            if (isTerminated()) {
+                termination.signalAll();
+            }
+        } finally {
+            lock.unlock();
+        }
     }
 }
